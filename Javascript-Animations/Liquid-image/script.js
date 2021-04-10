267dac5ce6c6a7e4d8dbdc639218cd80c1a49136
Ugurcan Turk <72524087+uct-trk@users.noreply.github.com>
2021-04-11 01:25:17 +0300

    Javascript animations

---
+++ Javascript-Animations/Liquid-image/script.js
@@ -0,0 +1,10 @@
+
+
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.5,
+    image1: './smoke.jpg',
+    image2: './smoke1.jpg',
+    /* angle: Math.PI / 8, */
+    displacementImage: './heightMap.png'
+});
